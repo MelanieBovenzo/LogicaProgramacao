e76327842cf30bbcbb909424d24431abea708ab6
shibo <melaniebovenzo@gmail.com>
2026-03-05 09:41:18 -0300

    IntroVariaveis

--- Project/Assets/Scripts/IntroVariaveis.cs
+++ Project/Assets/Scripts/IntroVariaveis.cs
@@ -1,16 +1,16 @@
 using UnityEngine;
 
-public class Player : MonoBehaviour
+public class IntroVariaveis : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        print("START");
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        print("UPDATE");
+        
     }
 }
